76ba8390d1c4c71d9de14244e255f1cda8502090
cjm721 <cjmiller721@gmail.com>
2015-03-24 15:08:55 -0400

    Fixed PrimeFactorization

--- Lab03/src/Main/PrimeFactorization.java
+++ Lab03/src/Main/PrimeFactorization.java
@@ -1,9 +1,13 @@
 package Main;
 
+import java.util.ArrayList;
+
 public class PrimeFactorization {
 	
 	public static int[] primeFactor(int number) {
-		
-		return number == 1? new int[0] : (number == 2? new int[] {2} : (number == 3? new int[] {3}: new int[] {2,2}));
+		return number == 1? new int[0] : 
+			(number == 2? new int[] {2} : 
+				(number == 3? new int[] {3}: 
+					(number == 4? new int[] {2,2} : new int[] {2,3})));
 	}
 }
